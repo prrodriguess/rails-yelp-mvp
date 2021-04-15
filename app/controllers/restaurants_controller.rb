84b5dbc6405eb4b63d6c41ebe46435712dd98c93
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-04-15 16:31:23 -0300

    updating Yelp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,30 @@
 class RestaurantsController < ApplicationController
     def index
-        
+        @restaurants = Restaurant.all
     end
 
     def show
-        
+        @restaurant = Restaurant.find(params[:id])
     end
 
     def new
-        
+        @restaurant = Restaurant.new
     end
 
     def create
-        
+        @restaurant = Restaurant.new(restaurant_params)
+        @restaurants.restaurant = @restaurant
+        @restaurant.save
+        redirect_to restaurant_path(@restaurant)
+    end
+
+    private
+
+    def find_restaurant
+        @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:content)
     end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
     validates :name, :address, :category, presence: true
     validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
     message: "%{value} is not a valid category" }
-    has_many :reviews, dependent:destroy
+    has_many :reviews, dependent: :destroy
 end
